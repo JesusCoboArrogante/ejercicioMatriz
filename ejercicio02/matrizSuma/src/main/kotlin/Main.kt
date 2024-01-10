e7b5457cbf08c550af6042401c63bd9f6f73546d
Jesus Cobo <jessucobo1997@gmail.com>
2024-01-10 14:52:20 +0100

    first commit

--- ejercicio02/matrizSuma/src/main/kotlin/Main.kt
+++ ejercicio02/matrizSuma/src/main/kotlin/Main.kt
@@ -1,10 +1,33 @@
 
 
 fun main(args: Array<String>) {
-    val matriz = arrayOf(arrayOf(11,12,13), arrayOf(21,22,23), arrayOf(31,32,33))
-    val newMatriz = arrayOf(arrayOf(0,0,0,0), arrayOf(0,0,0,0), arrayOf(0,0,0,0), arrayOf(0,0,0,0))
+    val matriz = arrayOf(
+        floatArrayOf(02.5f,-6.3f,14.7f,04.0f,0.0f),
+        floatArrayOf(10.8f,12.4f,-8.2f,05.5f,0.0f),
+        floatArrayOf(-7.2f,03.1f,17.7f,-9.1f,0.0f))
+
+    val newMatriz = arrayOf(
+        floatArrayOf(0.0f,0.0f,0.0f,0.0f,0.0f,0.0f),
+        floatArrayOf(0.0f,0.0f,0.0f,0.0f,0.0f,0.0f),
+        floatArrayOf(0.0f,0.0f,0.0f,0.0f,0.0f,0.0f),
+        floatArrayOf(0.0f,0.0f,0.0f,0.0f,0.0f,0.0f))
+
+    for (i in matriz.indices){
+        for (j in matriz.indices){
+            print("[${matriz[i][j]}]")
+        }
+        println()
+    }
 
     copia(matriz,newMatriz)
+    fila(newMatriz)
+    columna(newMatriz)
+
+
+
+    println("--------------------------------------")
+
+
     for (i in newMatriz.indices){
         for (j in newMatriz.indices){
             print("[${newMatriz[i][j]}]")
@@ -17,18 +40,14 @@ fun main(args: Array<String>) {
 }
 
 
-fun copia(matriz: Array<Array<Int>>, newMatriz: Array<Array<Int>>) {
+
+fun copia(matriz: Array<FloatArray>, newMatriz: Array<FloatArray>) {
     for (i in matriz.indices){
         for (j in matriz.indices){
-            if (matriz[i][j] == matriz[0][j]){
-                newMatriz[i][j] = matriz[i][j]
-            }
-            if (matriz[i][j] == matriz[1][j]){
-                newMatriz[i][j] = matriz[i][j]
-            }
-            if (matriz[i][j] == matriz[2][j]){
-                newMatriz[i][j] = matriz[i][j]
-            }
+           if (i < 4){
+               newMatriz[i][j] = matriz[i][j]
+           }
+
 
 
         }
@@ -37,10 +56,35 @@ fun copia(matriz: Array<Array<Int>>, newMatriz: Array<Array<Int>>) {
 
 }
 
-fun suma(newMatriz: Array<Array<Int>>){
+fun fila(newMatriz: Array<FloatArray>){
 
-    for (i in newMatriz)
+    for (i in newMatriz.indices){
+        for (j in newMatriz.indices){
+           if (j < 4){
+               newMatriz[i][4] += newMatriz[i][j]
+           }
+
+        }
+    }
 }
 
 
+fun columna(newMatriz: Array<FloatArray>) {
+
+    for (i in newMatriz.indices){
+        for (j in newMatriz.indices){
+            var prueba = 0.0f
+            if ( newMatriz[i][j] > 0 && j < 4 && i < 4 ){
+                newMatriz[3][j] += newMatriz[i][j]
+                prueba += newMatriz[i][j]
+
+
+            }else if(newMatriz[i][j] < 0){
+                newMatriz[3][j] -= newMatriz[i][j]
+            }
+
+
+        }
+    }
+}
 
