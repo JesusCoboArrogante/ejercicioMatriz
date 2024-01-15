3b353eeea57039bdcf1ca0d2ebb7d4e65262d975
Jesus Cobo <jessucobo1997@gmail.com>
2024-01-15 20:23:13 +0100

    first commit

---
+++ ejercicio07/noventa/src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+const val MAX = 5
+fun main(args: Array<String>) {
+val matrizLectura = arrayOf(arrayOf(1,2,3,4,5), arrayOf(6,7,8,9,10), arrayOf(11,12,13,14,15), arrayOf(16,17,18,19,20), arrayOf(21,22,23,24,25))
+    val matrizEscritura = Array(MAX) {IntArray(MAX)}
+
+    imprimir(matrizLectura)
+    println()
+    clonar(matrizLectura,matrizEscritura)
+
+
+
+}
+
+fun imprimir(matrizLectura: Array<Array<Int>>) {
+    for (i  in   matrizLectura.indices){
+        for (j in matrizLectura.indices){
+            print("[${matrizLectura[i][j]}]")
+        }
+        println()
+    }
+}
+
+fun clonar(matrizLectura: Array<Array<Int>>, matrizEscritura: Array<IntArray>) {
+    for (i in matrizEscritura.indices){
+        for (j in matrizEscritura.indices){
+            matrizEscritura[i][j]= matrizLectura[j][i]
+            print("[${matrizEscritura[i][j]}]")
+        }
+        println()
+    }
+}
